cb756b65a8585d4f954e933731fcca0193e4bc10
Bertrand Florat <bertrand@florat.net>
2026-08-11 21:42:57 +0200

    test: fix tests

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,7 +1,20 @@
 import '@testing-library/jest-dom/vitest'
 
+const storage = new Map()
+const localStorageMock = {
+  clear: () => storage.clear(),
+  getItem: (key) => storage.get(key) ?? null,
+  key: (index) => [...storage.keys()][index] ?? null,
+  removeItem: (key) => storage.delete(key),
+  setItem: (key, value) => storage.set(String(key), String(value)),
+  get length() { return storage.size }
+}
+
+// Node's experimental Storage global is unavailable in the test workers.
+Object.defineProperty(globalThis, 'localStorage', { configurable: true, value: localStorageMock })
+Object.defineProperty(window, 'localStorage', { configurable: true, value: localStorageMock })
+
 // Make sure tests start from a clean storage state
 beforeEach(() => {
-  localStorage.clear()
+  localStorageMock.clear()
 })
-
